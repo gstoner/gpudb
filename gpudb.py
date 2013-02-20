626fa5507ffaa3ee3aa2103c66027efac2851a03
yuanyuan0526@gmail.com <yuanyuan0526@gmail.com@ae7ad842-7f85-6292-3d62-d7cd969ac15c>
2013-02-20 20:36:37 +0000

    save for wrapper, not finish yet

--- gpudb.py
+++ gpudb.py
@@ -3,18 +3,21 @@
 import sys
 import os
 import shutil
+import pickle
+sys.path.insert(0, "XML2CODE")
+import ystree
 
 def dbHelp():
     print "Command:"
     print "\tcreate DBName: create the database"
     print "\tdelete DBName: delete the database"
-    print "\tlist: list the table infomation in the database"
+    print "\tlist DBName: list the table infomation in the database"
+    print "\tload TableName data: load data into the given table"
     print "\ttranslate SQL: translate SQL into CUDA file"
     print "\texecute SQL: translate and execute given SQL on GPU"
-    print "\tload TableName data: load data into the given table"
     print "\texit"
 
-def dbCreate(dbName):
+def dbCreate(dbName, schemaFile):
 
     ret = 0
     dbTop = "database"
@@ -29,7 +32,7 @@ def dbCreate(dbName):
 
     os.makedirs(dbPath)
 
-    cmd = 'python XML2MapReduce/main.py ' + schemaFile
+    cmd = 'python XML2CODE/main.py ' + schemaFile + ' &> /dev/null'
     ret = os.system(cmd)
 
     if ret !=0 :
@@ -41,7 +44,13 @@ def dbCreate(dbName):
     if ret != 0:
         exit(-1)
 
-    cmd = 'cp src/GPUCODE/gpuDBLoader ' + dbPath
+    cmd = 'mv src/GPUCODE/gpuDBLoader ' + dbPath
+    ret = os.system(cmd)
+
+    if ret != 0:
+        exit(-1)
+
+    cmd = 'mv src/GPUCODE/.metadata ' + dbPath
     ret = os.system(cmd)
 
     if ret != 0:
@@ -56,6 +65,26 @@ def dbDelete(dbName):
     dbPath = dbTop + "/" + dbName
     if os.path.exists(dbPath):
         shutil.rmtree(dbPath)
+
+def dbList(dbName):
+
+    dbTop = "database"
+    dbPath = dbTop + "/" + dbName
+
+    if not os.path.exists(dbPath):
+        return -1
+
+    metaPath = dbPath + "/.metadata" 
+
+    if not os.path.exists(metaPath):
+        return -2
+
+    metaFile = open(metaPath, 'rb')
+    tableDict = pickle.load(metaFile)
+    metaFile.close()
+
+    for tn in tableDict.keys():
+        print tn
 
 if len(sys.argv) != 2:
     print "./gpudb.py schemaFile"
@@ -94,22 +123,34 @@ while 1:
             print "usage: create DBName"
 
         else:
-            ret = dbCreate(cmdA[1].upper())
+            ret = dbCreate(cmdA[1].upper(), schemaFile)
             if ret == -1:
                 print cmdA[1] + " already exists"
             else:
                 dbCreated = 1
                 dbName = cmdA[1].upper()
-                print cmdA[1] + " has been successfully created"
+                print cmdA[1] + " has been successfully created."
                 
 
 
     elif cmdA[0].upper() == "DELETE":
-        if len(cmdA) !=2:
+        if len(cmdA) != 2:
             print "usage: delete DBName"
 
         dbCreated = 0
         dbDelete(cmdA[1].upper())
+
+        print cmdA[1] + " has been successfully deleted."
+
+    elif cmdA[0].upper() == "LIST":
+        if len(cmdA) != 2:
+            print "usage: list DBName"
+            continue
+
+        dbList(cmdA[1].upper())
+
+    elif cmdA[0].upper() == "LOAD":
+        pass
 
     else:
         print "Unknown command"
