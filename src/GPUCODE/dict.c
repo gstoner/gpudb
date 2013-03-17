57f4b2b231f0eadb68cb2995b3eec6d1315b8d92
yuanyuan0526@gmail.com <yuanyuan0526@gmail.com@ae7ad842-7f85-6292-3d62-d7cd969ac15c>
2013-03-17 14:55:57 +0000

    update dictCompression

--- src/GPUCODE/dict.c
+++ src/GPUCODE/dict.c
@@ -42,150 +42,174 @@ int main(int argc, char ** argv){
 		exit(-1);
 	}
 
-	header.format = DICT;
-	tupleNum = header.tupleNum;
-	size = tupleNum * sizeof(int);
 
-	char * content = (char *) malloc(size);
-	if(!content){
-		printf("Failed to allocate memory to accomodate the column\n");
-		exit(-1);
-	}
+	long tupleOffset = 0;
+	int blockTotal = header.blockTotal;
+	long offset = 0;
 
-	char *table = (char *) mmap(0,size + sizeof(struct columnHeader),PROT_READ,MAP_SHARED,inFd,0);
-	memcpy(content, table + sizeof(struct columnHeader), size);
-	munmap(table,size + sizeof(struct columnHeader));
+	for(int j=0;j<blockTotal;j++){
+		offset = j* sizeof(struct columnHeader) + tupleOffset * sizeof(int);
+		lseek(inFd,offset,SEEK_SET);
+		read(inFd,&header, sizeof(struct columnHeader));
+		header.format = DICT;
+		offset += sizeof(struct columnHeader);
 
-	close(inFd);
+		tupleNum = header.tupleNum;
+		size = tupleNum * sizeof(int);
+		char * content = (char *) malloc(size);
 
-	int hashTable[HSIZE] ;
+		if(!content){
+			printf("Failed to allocate memory to accomodate the column\n");
+			exit(-1);
+		}
 
-	memset(hashTable,-1,sizeof(int) * HSIZE);
+		char *table = (char *) mmap(0,size,PROT_READ,MAP_SHARED,inFd,offset);
+		memcpy(content, table, size);
+		munmap(table,size);
+		close(inFd);
 
-	for(int i=0;i<tupleNum;i++){
+		int hashTable[HSIZE] ;
+		memset(hashTable,-1,sizeof(int) * HSIZE);
 
-		int key = ((int *)content)[i];
-		int hKey = key % HSIZE;
+		for(int i=0;i<tupleNum;i++){
 
-		if(hashTable[hKey] == -1){
-			numOfDistinct ++;
-			hashTable[hKey] = key;
-		}else{
-			if(hashTable[hKey] == key)
-				continue;
-
-			int j = 1;
-			while(hashTable[hKey] != -1 && hashTable[hKey] != key){
-				hKey = key % (HSIZE + j*111) % HSIZE; 
-				j = j+1;
-			}
+			int key = ((int *)content)[i];
+			int hKey = key % HSIZE;
 
 			if(hashTable[hKey] == -1){
-				hashTable[hKey] = key;
 				numOfDistinct ++;
+				hashTable[hKey] = key;
+			}else{
+				if(hashTable[hKey] == key)
+					continue;
+
+				int j = 1;
+				while(hashTable[hKey] != -1 && hashTable[hKey] != key){
+					hKey = key % (HSIZE + j*111) % HSIZE; 
+					j = j+1;
+				}
+
+				if(hashTable[hKey] == -1){
+					hashTable[hKey] = key;
+					numOfDistinct ++;
+				}
 			}
 		}
-	}
 
-	if(numOfDistinct > MAX_DICT_NUM)
-		goto END;
+		if(numOfDistinct > MAX_DICT_NUM)
+			goto END;
 
-	int numOfBits =1 ;
+		int numOfBits =1 ;
 
-	// the number of bits needed to encode all the distinct values
-	while((1 << numOfBits) < numOfDistinct){
-		numOfBits ++;
-	}
-
-	// align on byte
-	while(numOfBits % 8 !=0)
-		numOfBits ++;
-
-	// if the number of bits is larger than the bits in an int type, donot compress
-	if(numOfBits >= sizeof(int) * 8)
-		goto END;
-
-	// make sure that one int type can accomodate multple compressed values
-	int stride = sizeof(int) * 8 / numOfBits;
-
-	if(stride <= 1)
-		goto END;
-
-	dHeader.dictNum = numOfDistinct;
-	dHeader.bitNum = numOfBits;
-
-	int * result = (int *) malloc(sizeof(int) * numOfDistinct);
-	if(!result){
-		printf("failed to allocate memory for result hash\n");
-		exit(-1);
-	}
-
-	memset(result, -1, sizeof(int) * numOfDistinct);
-
-	for(int i=0; i<HSIZE;i++){
-		if(hashTable[i] == -1)
-			continue;
-
-		int key = hashTable[i];
-		int hKey = key % numOfDistinct;
-		if( result[hKey] == -1){
-			result[hKey] = key;
-		}else{
-			int j = 1;
-			while(result[hKey] !=-1){
-				hKey = key % (numOfDistinct + 111*j) % numOfDistinct;
-				j++;
-			}
-			result[hKey] = key;
+		while((1 << numOfBits) < numOfDistinct){
+			numOfBits ++;
 		}
-	}
 
-	for(int i=0;i<numOfDistinct;i++){
-		dHeader.hash[i] = result[i];
-	}
+		while(numOfBits % 8 !=0)
+			numOfBits ++;
 
+		if(numOfBits >= sizeof(int) * 8)
+			goto END;
 
-	int bitInInt = sizeof(int) * 8/ stride;
+		int stride = sizeof(int) * 8 / numOfBits;
 
-	outFd = open(argv[2],O_RDWR|O_CREAT);
-	if(outFd == -1){
-		printf("Failed to create output column\n");
-		exit(-1);
-	}
+		if(stride <= 1)
+			goto END;
 
-	write(outFd, &header, sizeof(struct columnHeader));
-	write(outFd, &dHeader, sizeof(struct dictHeader));
+		dHeader.dictNum = numOfDistinct;
+		dHeader.bitNum = numOfBits;
 
-	for(int i=0; i<tupleNum; i+= stride){
+		int * result = (int *) malloc(sizeof(int) * numOfDistinct);
+		if(!result){
+			printf("failed to allocate memory for result hash\n");
+			exit(-1);
+		}
 
-		int outInt = 0;
+		memset(result, -1, sizeof(int) * numOfDistinct);
 
-		for(int k=0;k<stride;k++){
-			if((i+k) >= tupleNum)
-				break;
+		for(int i=0; i<HSIZE;i++){
+			if(hashTable[i] == -1)
+				continue;
 
-			int key = ((int *)content)[k+i];
+			int key = hashTable[i];
 			int hKey = key % numOfDistinct;
+			if( result[hKey] == -1){
+				result[hKey] = key;
+			}else{
+				int j = 1;
+				while(result[hKey] !=-1){
+					hKey = key % (numOfDistinct + 111*j) % numOfDistinct;
+					j++;
+				}
+				result[hKey] = key;
+			}
+		}
 
-			int j = 1;
-			while(result[hKey] != key){
-				hKey = key % (numOfDistinct + 111 * j) % numOfDistinct;
-				j++;
+		for(int i=0;i<numOfDistinct;i++){
+			dHeader.hash[i] = result[i];
+		}
+
+
+		int bitInInt = sizeof(int) * 8/ stride;
+
+		outFd = open(argv[2],O_RDWR|O_CREAT);
+		if(outFd == -1){
+			printf("Failed to create output column\n");
+			exit(-1);
+		}
+
+		long outOffset = 0;
+
+		int * tmp = (int *) malloc(sizeof(int) * tupleNum);
+
+		for(int i=0; i<tupleNum; i+= stride){
+
+			int outInt = 0;
+
+			for(int k=0;k<stride;k++){
+				if((i+k) >= tupleNum)
+					break;
+
+				int key = ((int *)content)[k+i];
+				int hKey = key % numOfDistinct;
+
+				int j = 1;
+				while(result[hKey] != key){
+					hKey = key % (numOfDistinct + 111 * j) % numOfDistinct;
+					j++;
+				}
+
+				hKey = hKey & 0xFFFF;
+				memcpy((char *)(&outInt) + k*dHeader.bitNum/8, &hKey, dHeader.bitNum/8);
+
 			}
 
-			hKey = hKey & 0xFFFF;
-			memcpy((char *)(&outInt) + k*dHeader.bitNum/8, &hKey, dHeader.bitNum/8);
+			tmp[outOffset] = outInt;
+			outOffset += 1; 
 
 		}
 
-		write(outFd, &outInt, sizeof(int));
+		long blockSize = (4095 + sizeof(struct dictHeader) + outOffset * sizeof(int)) /4096 * 4096;
+		header.blockSize = blockSize;
 
+		write(outFd, &header, sizeof(struct columnHeader));
+		write(outFd, &dHeader, sizeof(struct dictHeader));
+		write(outFd,tmp,outOffset*sizeof(int));
+
+		char buf[4096];
+		memset(buf,0,sizeof(buf));
+		int paddingSize = blockSize - sizeof(struct dictHeader) - outOffset * sizeof(int); 
+		write(outFd, buf, paddingSize);
+
+		close(outFd);
+		free(content);
+		free(tmp);
+		free(result);
+
+		tupleOffset += tupleNum;
 	}
-
-	close(outFd);
 
 END:
-	free(content);
 
 	return res;
 }
