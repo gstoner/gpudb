f4bb23e9c6a2a1c071aafc7f42d877e2e39975ea
yuanyuan0526@gmail.com <yuanyuan0526@gmail.com@ae7ad842-7f85-6292-3d62-d7cd969ac15c>
2013-07-18 12:45:54 +0000

    update

--- src/include/gpuCudaLib.h
+++ src/include/gpuCudaLib.h
@@ -1,3 +1,19 @@
+/*
+    Copyright (c) 2012-2013 The Ohio State University.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 #ifndef __GPU_CUDALIB_H_
 #define __GPU_CUDALIB_H_
 
@@ -26,7 +42,7 @@
 #define CUDA_SAFE_CALL_NO_SYNC_RETRY( call) do {                                \
     cudaError err = call;                                                    \
     while( cudaSuccess != err) {                                                \
-	err = call;								\
+    err = call;                             \
     } } while (0)
 
 static size_t getGpuGlobalMem(int deviceID){
@@ -37,15 +53,15 @@ static size_t getGpuGlobalMem(int deviceID){
 }
 
 static long getAvailMem(void){
-	long mem = sysconf(_SC_AVPHYS_PAGES) * sysconf(_SC_PAGE_SIZE);
-	return mem;
+    long mem = sysconf(_SC_AVPHYS_PAGES) * sysconf(_SC_PAGE_SIZE);
+    return mem;
 }
 
 static double getCurrentTime(void){
-	struct timeval tv;
+    struct timeval tv;
         gettimeofday(&tv, NULL);
         double curr  = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000 ;
-	return curr;
+    return curr;
 }
 
 #endif
